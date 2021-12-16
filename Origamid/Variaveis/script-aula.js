b63e0615650ba9c46863cd0e148f4e0b81e61283
Gustavo <gustavo@imaginative.company>
2021-12-16 08:10:21 -0300

    New State

---
+++ Origamid/Variaveis/script-aula.js
@@ -0,0 +1,16 @@
+// Declarar uma variável com o seu nome
+var nome = Gustavo;
+
+// Declarar uma variável com a sua idade
+var idade = 24;
+
+// Declarar uma variável com a sua comida
+// favorita e não atribuir valor
+
+var comidaFavorita;
+
+// Atribuir valor a sua comida favorita
+comidaFavorita = Lanche;
+
+// Declarar 5 variáveis diferentes sem valores
+var time, professor, pais, endereco, rua;
---
+++ Origamid/Variaveis/script.js
@@ -0,0 +1,16 @@
+var nome = 'Gustavo';
+var idade = 24;
+var possuiFaculdade = false;
+
+console.log(nome, idade, possuiFaculdade);
+
+var preco = 25;
+var totalComprado = 5;
+var totalPreco = totalComprado * preco;
+
+console.log(totalPreco);
+
+var sobrenome = 'Gustavo',
+  cidade = 'Rio';
+
+console.log(sobrenome, cidade);
