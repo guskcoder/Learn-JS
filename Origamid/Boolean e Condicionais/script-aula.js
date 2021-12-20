53b442316e443cd8a3c4a4e261292b9cf5d62bd4
Gustavo <gustavo@imaginative.company>
2021-12-20 10:54:07 -0300

    Bolean Js

---
+++ Origamid/Boolean e Condicionais/script-aula.js
@@ -0,0 +1,24 @@
+// // var possuiGraducao = true;
+// // var possuiDoutorado = false;
+
+// // if (possuiGraducao) {
+// //   console.log('E Verdadeiro');
+// // } else if (possuiDoutorado) {
+// //   console.log('E falso');
+// // }
+
+// // if (5 - 10 && 5 + 5) {
+// //   console.log('Verdadeiro');
+// // } else {
+// //   console.log('Falso');
+// // }
+
+// var corFavorita = 'Azul';
+
+// switch (corFavorita) {
+//   case 'Azul':
+//     console.log('Olhe para o ceu.');
+//     break;
+//   default:
+//     console.log('feches os olhos.');
+// }
