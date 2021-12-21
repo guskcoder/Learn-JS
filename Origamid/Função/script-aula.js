d83d03a793077d693b107ae6a715eff82ea989d7
Gustavo <gustavo@imaginative.company>
2021-12-20 22:48:53 -0300

    Function Js

---
+++ Origamid/Função/script-aula.js
@@ -0,0 +1,22 @@
+// function imc(peso, altura) {
+//   var imc = peso / (altura * altura);
+//   return imc;
+// }
+
+// console.log(imc(80, 1.3));
+
+// function corFavorita(cor) {
+//   if (cor === 'azul') {
+//     return 'Eu gosto do Ceu';
+//   } else if (cor === 'verde') {
+//     return 'Eu gosto do mato';
+//   } else {
+//     return 'Eu nao gosto do cores';
+//   }
+// }
+
+// function mostraOi() {
+//   alert('Oi');
+// }
+
+// addEventListener('click', mostraOi);
