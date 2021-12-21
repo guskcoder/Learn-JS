0e3ef1d0e99370aaaad2ab901dadd5a9f84fcd9a
Gustavo Henrique <gustavo@imaginative.company>
2021-12-21 08:22:40 -0300

    Function Exercice

--- Origamid/Função/exercicio-aula.js
+++ Origamid/Função/exercicio-aula.js
@@ -1,24 +1,49 @@
 // Crie uma função para verificar se um valor é Truthy
+function isTrufy(dados) {
+  return !!dados;
+}
 
 // Crie uma função matemática que retorne o perímetro de um quadrado
 // lembrando: perímetro é a soma dos quatro lados do quadrado
+function perimetroQuadrado(lado) {
+  return lado * 4;
+}
 
 // Crie uma função que retorne o seu nome completo
 // ela deve possuir os parâmetros: nome e sobrenome
+function nomeCompleto(nome, sobrenome) {
+  return `${nome} ${sobrenome}`;
+}
 
 // Crie uma função que verifica se um número é par
+function par(numero) {
+  var modulo = numero % 2;
+  if (modulo === 0) {
+    return true;
+  } else {
+    return numero / 2;
+  }
+}
 
 // Crie uma função que retorne o tipo de
 // dado do argumento passado nela (typeof)
+function tipoDeDado(dado) {
+  return typeof dado;
+}
 
 // addEventListener é uma função nativa do JavaScript
 // o primeiro parâmetro é o evento que ocorre e o segundo o Callback
 // utilize essa função para mostrar no console o seu nome completo
 // quando o evento 'scroll' ocorrer.
+addEventListener('scroll', function () {
+  console.log('Gustavo Henrique');
+});
 
-// Corrija o erro abaixo
+// // Corrija o erro abaixo
+
+var totalPaises = 193;
+
 function precisoVisitar(paisesVisitados) {
-  var totalPaises = 193;
   return `Ainda faltam ${totalPaises - paisesVisitados} países para visitar`;
 }
 function jaVisitei(paisesVisitados) {
