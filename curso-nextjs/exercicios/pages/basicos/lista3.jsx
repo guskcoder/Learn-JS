f82c338e90bc48b5826d7bdb666c2e9f0a41695f
Gustavo <gustavo@imaginative.company>
2022-01-02 10:51:39 -0300

    Next JS #day 4

---
+++ curso-nextjs/exercicios/pages/basicos/lista3.jsx
@@ -0,0 +1,11 @@
+function gerarLista(final = 10) {
+  const lista = [];
+  for (let i = 1; i <= final; i++) {
+    lista.push(<span>{i},</span>);
+  }
+  return lista;
+}
+
+export default function lista() {
+  return <div>{gerarLista(50)}</div>;
+}
