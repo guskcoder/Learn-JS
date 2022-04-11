9edd31a9943ab8fdcf2c28c7376d9f357a5c9056
Gustavo Henrique <gustavo@imaginative.company>
2022-04-11 08:03:10 -0300

    React Learn 3

---
+++ Discover/reactapp/src/components/Card/index.jsx
@@ -0,0 +1,10 @@
+import './style.css';
+
+export function Card() {
+  return (
+    <div className="card">
+      <strong>Gustavo Henrique</strong>
+      <small>10:15:24</small>
+    </div>
+  );
+}
--- Discover/reactapp/src/pages/Home/index.jsx
+++ Discover/reactapp/src/pages/Home/index.jsx
@@ -1,11 +1,17 @@
 import './style.css';
 
+import { Card } from '../../components/Card';
+
 export function Home() {
   return (
     <div className="container">
       <h1>Lista de Presença</h1>
       <input type="text" placeholder="Digite o nome" />
       <button type="button">Adicionar</button>
+
+      <Card />
+      <Card />
+      <Card />
     </div>
   );
 }
