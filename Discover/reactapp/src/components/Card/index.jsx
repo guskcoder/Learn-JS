4ede7e2bb5a9589eb43c0feef9e7a4985cd6bd64
Gustavo Henrique <gustavo@imaginative.company>
2022-04-11 12:18:42 -0300

    React Learn Finish

--- Discover/reactapp/src/components/Card/index.jsx
+++ Discover/reactapp/src/components/Card/index.jsx
@@ -1,10 +1,10 @@
 import './style.css';
 
-export function Card() {
+export function Card(props) {
   return (
     <div className="card">
-      <strong>Gustavo Henrique</strong>
-      <small>10:15:24</small>
+      <strong>{props.name}</strong>
+      <small>{props.time}</small>
     </div>
   );
 }
--- Discover/reactapp/src/pages/Home/index.jsx
+++ Discover/reactapp/src/pages/Home/index.jsx
@@ -1,17 +1,62 @@
+import React, { useState, useEffect } from 'react';
 import './style.css';
 
 import { Card } from '../../components/Card';
 
 export function Home() {
+  const [studentName, setStudentName] = useState();
+  const [students, setStudents] = useState([]);
+  const [user, setUser] = useState({ name: '', avatar: '' });
+
+  function handleAddStudent() {
+    const newStudent = {
+      name: studentName,
+      time: new Date().toLocaleDateString('pt-br', {
+        hour: '2-digit',
+        minute: '2-digit',
+        second: '2-digit',
+      }),
+    };
+    setStudents((prevState) => [...prevState, newStudent]);
+  }
+
+  useEffect(() => {
+    async function fecthData() {
+      const response = await fetch('https://api.github.com/users/guskcoder');
+
+      const data = await response.json();
+
+      setUser({
+        name: data.name,
+        avatar: data.avatar_url,
+      });
+    }
+
+    fecthData();
+  }, []);
+
   return (
     <div className="container">
-      <h1>Lista de Presença</h1>
-      <input type="text" placeholder="Digite o nome" />
-      <button type="button">Adicionar</button>
+      <header>
+        <h1>Lista de Presença</h1>
+        <div>
+          <strong>{user.name}</strong>
+          <img src={user.avatar} alt="Foto de Perfil" />
+        </div>
+      </header>
 
-      <Card />
-      <Card />
-      <Card />
+      <input
+        type="text"
+        placeholder="Digite o nome"
+        onChange={(e) => setStudentName(e.target.value)}
+      />
+      <button type="button" onClick={handleAddStudent}>
+        Adicionar
+      </button>
+
+      {students.map((student) => (
+        <Card key={student.time} name={student.name} time={student.time} />
+      ))}
     </div>
   );
 }
