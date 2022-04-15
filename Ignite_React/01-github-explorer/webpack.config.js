2ea8e3e34349aff0d386bbb65172c0741643ca77
Gustavo Henrique <gustavo@imaginative.company>
2022-04-14 21:14:14 -0300

    React Learn 5

--- Ignite_React/01-github-explorer/webpack.config.js
+++ Ignite_React/01-github-explorer/webpack.config.js
@@ -1,14 +1,26 @@
 const path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
 
 module.exports = {
+  mode: 'development',
   entry: path.resolve(__dirname, 'src', 'index.jsx'),
   output: {
     path: path.resolve(__dirname, 'dist'),
     filename: 'bundle.js',
   },
   resolve: {
-    extensions: ['/js', '.jsx'],
+    extensions: ['.js', '.jsx'],
   },
+  devServer: {
+    static: {
+      directory: path.resolve(__dirname, 'public'),
+    },
+  },
+  plugins: [
+    new HtmlWebpackPlugin({
+      template: path.resolve(__dirname, 'public', 'index.html'),
+    }),
+  ],
   module: {
     rules: [
       {
