1f48c40a82d6edac16b774edbdb9e494f3b4cd5a
Gustavo Henrique <gustavo@imaginative.company>
2022-04-29 16:52:25 -0300

    :tada: Code 14

--- Origamid-React/meuapp/src/App.js
+++ Origamid-React/meuapp/src/App.js
@@ -1,12 +1,21 @@
 import React from 'react';
 import Header from './Header';
 import Home from './Home';
+import Produtos from './Produtos';
 
 const App = () => {
+  const { pathname } = window.location;
+  let Pagina;
+  if (pathname === '/produtos') {
+    Pagina = Produtos;
+  } else {
+    Pagina = Home;
+  }
+
   return (
     <section>
       <Header />
-      <Home />
+      <Pagina />
     </section>
   );
 };
