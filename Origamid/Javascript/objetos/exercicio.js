7ed04a1f1b13ad62643c8e237daa2bf6b3172128
Gustavo Henrique <gustavo@imaginative.company>
2022-05-04 17:54:23 -0300

    :tada: Code 22

--- Origamid/Javascript/objetos/exercicio.js
+++ Origamid/Javascript/objetos/exercicio.js
@@ -1,7 +1,20 @@
 // Crie um objeto com os seus dados pessoais
 // Deve possui pelo menos duas propriedades nome e sobrenome
 
+var dados = {
+  nome: 'Gustavo',
+  sobrenome: 'Henrique',
+  idade: 25,
+  cidade: 'Rio Verde',
+};
+
 // Crie um método no objeto anterior, que mostre o seu nome completo
+
+dados.nomeCompleto = function () {
+  return `${this.nome} ${this.sobrenome}`;
+};
+
+console.log(dados.nomeCompleto());
 
 // Modifique o valor da propriedade preco para 3000
 var carro = {
@@ -10,5 +23,20 @@ var carro = {
   marca: 'Audi',
 };
 
+carro.preco = 3000;
+
 // Crie um objeto de um cachorro que represente um labrador,
 // preto com 10 anos, que late ao ver um homem
+
+var cachorro = {
+  raca: 'Labrador',
+  cor: 'preto',
+  idade: 10,
+  latir(pessoa) {
+    if (pessoa === 'homem') {
+      return 'Latir';
+    } else {
+      return 'Nada';
+    }
+  },
+};
