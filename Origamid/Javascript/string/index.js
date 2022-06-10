c6095e1e942e6679de4aee6ff60ff8540f1807de
Gustavo Henrique <gustavo@imaginative.company>
2022-06-10 12:22:21 -0300

    :tada: Code 29

---
+++ Origamid/Javascript/string/index.js
@@ -0,0 +1,11 @@
+const fruta = 'Banana';
+
+const listaFrutas = 'Melancia, Banana, Laranja';
+
+console.log(listaFrutas.includes(fruta));
+
+let valor = 48.49;
+
+valor = valor.toLocaleString('pt-BR', { style: 'currency', currency: 'BRL' });
+
+console.log(valor);
