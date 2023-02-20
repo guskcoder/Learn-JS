219e5cfcd4ae0cbdd491801676ab8b2ad9ad62a7
Gustavo <gustavo@imaginative.company>
2023-02-20 11:29:56 -0300

    studies js day 13

---
+++ B7Web/Javascript Novo/Orientação a Objetos/script.js
@@ -0,0 +1,12 @@
+class Person {
+  constructor(name, age) {
+    this.name = name;
+    this.age = age;
+  }
+}
+
+let p1 = new Person('Gustavo', 26);
+let p2 = new Person('Maria', 30);
+let p3 = new Person('Pedro', 40);
+
+console.log(p1.name);
